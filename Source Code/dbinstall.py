5e442fc00d7837eebd384c8f690d22009df5edca
ravigohel99 <47741834+ravigohel99@users.noreply.github.com>
2020-02-03 14:15:08 +0000

    Created tables

--- Source Code/dbinstall.py
+++ Source Code/dbinstall.py
@@ -1,8 +1,18 @@
 import sqlite3
+import datetime
 
 conn = sqlite3.connect('treasure.db')
-print "Opened database successfully";
+print ("Opened database successfully");
 
-conn.execute('CREATE TABLE Teams ()')
-print "Table created successfully";
+conn.execute('CREATE TABLE IF NOT EXISTS Users (UserID integer PRIMARY KEY, TeamID int not null, Name string not null, FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+print ("Table created successfully");
+
+conn.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID int PRIMARY KEY, TeamName string not null, Password string not null, Subject string not null)')
+print ("Table created successfully");
+
+conn.execute('CREATE TABLE IF NOT EXISTS Results (ResultID int PRIMARY KEY, ClueID int not null, TeamID int not null, Result string not null, TimeTaken datetime not null, FOREIGN KEY (ClueID) REFERENCES Clue (ClueID), FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+print ("Table created successfully");
+
+conn.execute('CREATE TABLE IF NOT EXISTS Clues (ClueID int PRIMARY KEY, Subject string not null, Question string not null, Hint string not null, Answer string not null)')
+print ("Table created successfully");
 conn.close()
