9a87b447ffc56f65ef2847e6ebb3e73aa8f9debc
ravigohel99 <47741834+ravigohel99@users.noreply.github.com>
2020-02-04 13:20:56 +0000

    Updated Database to New Design

--- Source Code/dbinstall.py
+++ Source Code/dbinstall.py
@@ -4,15 +4,28 @@ import datetime
 conn = sql.connect('treasure.db')
 print ("Opened database successfully");
 
-conn.execute('CREATE TABLE IF NOT EXISTS Users (UserID integereger PRIMARY KEY AUTOINCREMENT, TeamID integer not null, Name string not null, FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+#Tutors Table
+conn.execute('CREATE TABLE IF NOT EXISTS Tutors (TutorID integer PRIMARY KEY AUTOINCREMENT, Name string not null, Room int not null)')
 print ("Table created successfully");
 
-conn.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName string not null UNIQUE, Password string not null, Subject string not null)')
+#Keepers Table
+conn.execute('CREATE TABLE IF NOT EXISTS Keepers (KeeperID integer PRIMARY KEY AUTOINCREMENT, Name string not null, Username string not null UNIQUE, Password string not null)')
 print ("Table created successfully");
 
-conn.execute('CREATE TABLE IF NOT EXISTS Results (ResultID integer PRIMARY KEY AUTOINCREMENT, ClueID integer not null, TeamID integer not null, Result string not null, TimeTaken datetime not null, FOREIGN KEY (ClueID) REFERENCES Clue (ClueID), FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+#Games Table
+    #For active, as sqlite3 does not have a boolean data type - 1 will be true and 0 will be false
+conn.execute('CREATE TABLE IF NOT EXISTS Games (GameID integer PRIMARY KEY AUTOINCREMENT, Subject string not null, GamePin integer not null, Issuer integer not null, Active integer not null)')
 print ("Table created successfully");
 
-conn.execute('CREATE TABLE IF NOT EXISTS Clues (ClueID integer PRIMARY KEY AUTOINCREMENT, Subject string not null, Question string not null, Hint integer string not null, Answer string not null)')
+#Teams Table
+conn.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName string not null UNIQUE, Subject string not null, GamePin integer not null)')
+print ("Table created successfully");
+
+#Results Table
+conn.execute('CREATE TABLE IF NOT EXISTS Results (ResultID integer PRIMARY KEY AUTOINCREMENT, TeamID integer not null, StartTime datetime not null, FinishTime datetime, Letters int not null, FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+print ("Table created successfully");
+
+#Questions Table
+conn.execute('CREATE TABLE IF NOT EXISTS Questions (QuestionID integer PRIMARY KEY AUTOINCREMENT, Subject string not null, Location string not null, QRLocation string not null, QRString string not null, Question string not null, Answer string not null, GPS string not null, Letter string not null)')
 print ("Table created successfully");
 conn.close()
