9559d63c397c86f45877d9fe9226b86cae1843a8
ravigohel99 <47741834+ravigohel99@users.noreply.github.com>
2020-02-04 13:25:49 +0000

    Changes to DB

--- Source Code/dbinstall.py
+++ Source Code/dbinstall.py
@@ -5,20 +5,20 @@ conn = sql.connect('treasure.db')
 print ("Opened database successfully");
 
 #Tutors Table
-conn.execute('CREATE TABLE IF NOT EXISTS Tutors (TutorID integer PRIMARY KEY AUTOINCREMENT, Name string not null, Room int not null)')
+conn.execute('CREATE TABLE IF NOT EXISTS Tutors (TutorID integer PRIMARY KEY AUTOINCREMENT, Subject text not null, Name text not null, Room int not null)')
 print ("Table created successfully");
 
 #Keepers Table
-conn.execute('CREATE TABLE IF NOT EXISTS Keepers (KeeperID integer PRIMARY KEY AUTOINCREMENT, Name string not null, Username string not null UNIQUE, Password string not null)')
+conn.execute('CREATE TABLE IF NOT EXISTS Keepers (KeeperID integer PRIMARY KEY AUTOINCREMENT, Name text not null, Username text not null UNIQUE, Password text not null, Salt text not null)')
 print ("Table created successfully");
 
 #Games Table
     #For active, as sqlite3 does not have a boolean data type - 1 will be true and 0 will be false
-conn.execute('CREATE TABLE IF NOT EXISTS Games (GameID integer PRIMARY KEY AUTOINCREMENT, Subject string not null, GamePin integer not null, Issuer integer not null, Active integer not null)')
+conn.execute('CREATE TABLE IF NOT EXISTS Games (GameID integer PRIMARY KEY AUTOINCREMENT, Subject text not null, GamePin integer not null, Issuer integer not null, Active integer not null)')
 print ("Table created successfully");
 
 #Teams Table
-conn.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName string not null UNIQUE, Subject string not null, GamePin integer not null)')
+conn.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName text not null UNIQUE, Subject text not null, GamePin integer not null)')
 print ("Table created successfully");
 
 #Results Table
@@ -26,6 +26,6 @@ conn.execute('CREATE TABLE IF NOT EXISTS Results (ResultID integer PRIMARY KEY A
 print ("Table created successfully");
 
 #Questions Table
-conn.execute('CREATE TABLE IF NOT EXISTS Questions (QuestionID integer PRIMARY KEY AUTOINCREMENT, Subject string not null, Location string not null, QRLocation string not null, QRString string not null, Question string not null, Answer string not null, GPS string not null, Letter string not null)')
+conn.execute('CREATE TABLE IF NOT EXISTS Questions (QuestionID integer PRIMARY KEY AUTOINCREMENT, Subject text not null, Location text not null, QRLocation text not null, QRtext text not null, Question text not null, Answer text not null, GPS text not null, Letter text not null)')
 print ("Table created successfully");
 conn.close()
