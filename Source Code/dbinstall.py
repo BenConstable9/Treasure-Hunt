c055241f6900f52fd2aaa954ca1aab925c772ce8
ravigohel99 <47741834+ravigohel99@users.noreply.github.com>
2020-02-12 11:15:25 +0000

    Fixed Integer issues in DB

--- Source Code/dbinstall.py
+++ Source Code/dbinstall.py
@@ -11,7 +11,7 @@ print ("Opened database successfully");
 cur = con.cursor()
 
 #Tutors Table
-cur.execute('CREATE TABLE IF NOT EXISTS Tutors (TutorID integer PRIMARY KEY AUTOINCREMENT, SubjectID int not null, Name text not null, Room text not null, FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
+cur.execute('CREATE TABLE IF NOT EXISTS Tutors (TutorID integer PRIMARY KEY AUTOINCREMENT, SubjectID integer not null, Name text not null, Room text not null, FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
 print ("(1) Tutors Table created successfully");
 
 #Subjects Table
@@ -24,19 +24,19 @@ print ("(3) Keepers Table created successfully");
 
 #Games Table
     #For 'active', as sqlite3 does not have a boolean data type - 1 will be true and 0 will be false
-cur.execute('CREATE TABLE IF NOT EXISTS Games (GameID integer PRIMARY KEY AUTOINCREMENT, SubjectID int not null, GamePin text not null, KeeperID integer not null, Active integer not null, FOREIGN KEY (KeeperID) REFERENCES Keepers (KeeperID), FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
+cur.execute('CREATE TABLE IF NOT EXISTS Games (GameID integer PRIMARY KEY AUTOINCREMENT, SubjectID integer not null, GamePin text not null, KeeperID integer not null, Active integer not null, FOREIGN KEY (KeeperID) REFERENCES Keepers (KeeperID), FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
 print ("(4) Games Table created successfully");
 
 #Teams Table
-cur.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName text not null UNIQUE, SubjectID int not null, GamePin text not null, TutorID int not null, FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID), FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
+cur.execute('CREATE TABLE IF NOT EXISTS Teams (TeamID integer PRIMARY KEY AUTOINCREMENT, TeamName text not null UNIQUE, SubjectID integer not null, GamePin text not null, TutorID integer not null, FOREIGN KEY (TutorID) REFERENCES Tutors(TutorID), FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
 print ("(5) Teams Table created successfully");
 
 #Results Table
-cur.execute('CREATE TABLE IF NOT EXISTS Results (ResultID integer PRIMARY KEY AUTOINCREMENT, TeamID integer not null, StartTime datetime not null, FinishTime datetime, Letters int not null, FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
+cur.execute('CREATE TABLE IF NOT EXISTS Results (ResultID integer PRIMARY KEY AUTOINCREMENT, TeamID integer not null, StartTime datetime not null, FinishTime datetime, Letters integer not null, FOREIGN KEY (TeamID) REFERENCES Teams (TeamID))')
 print ("(6) Results Table created successfully");
 
 #Questions Table
-cur.execute('CREATE TABLE IF NOT EXISTS Questions (QuestionID integer PRIMARY KEY AUTOINCREMENT, SubjectID int not null, Building text not null, QRLocation text not null, QRText text not null, Question text not null, Answer text not null, Latitude text not null, Longitude text not null, Letter text not null, LetterIndex integer not null, FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
+cur.execute('CREATE TABLE IF NOT EXISTS Questions (QuestionID integer PRIMARY KEY AUTOINCREMENT, SubjectID integer not null, Building text not null, QRLocation text not null, QRText text not null, Question text not null, Answer text not null, Latitude text not null, Longitude text not null, Letter text not null, LetterIndex integer not null, FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))')
 print ("(7) Questions Table created successfully");
 
 cur = con.cursor()
