e8b14c5439ee2a2fa6dd0bbf09ee9f2561c7b624
BenConstable9 <ben-constable@outlook.com>
2020-02-16 19:02:14 +0000

    Fix the row factory issue

--- Technical-Documents/Source-Code/Helpers/utility.py
+++ Technical-Documents/Source-Code/Helpers/utility.py
@@ -9,3 +9,7 @@ from jinja2 import utils
 :return: A santized string. """
 def escapeInput(input):
     return str(utils.escape(input))
+
+def makeRowDictionary(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
