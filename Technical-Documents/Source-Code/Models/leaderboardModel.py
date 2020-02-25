c35b90925966214d159825b92fdd1d4b9375a1b9
adam-bannister <adambannister@ymail.com>
2020-02-25 13:54:45 +0000

    Leaderboard beginnings

--- Technical-Documents/Source-Code/Models/leaderboardModel.py
+++ Technical-Documents/Source-Code/Models/leaderboardModel.py
@@ -8,12 +8,32 @@ class leaderboardModel():
     def __init__(self):
         pass
 
-    def obtainResults(self):
+    """Fetch the relevant data for results for the leaderboard
+
+    :param gamePin: the gamePin to create results for
+
+    :return: a json response containing leaderboard data"""
+
+    def obtainResults(self, gamePin):
         #try the sql
         try:
             # Open the DB
             with sql.connect("Models/treasure.sqlite") as con:
-                con.row_factory = sql.Row
+                #map the column names to the values returned
+                con.row_factory = makeRowDictionary
                 cur = con.cursor()
 
                 # Get the appropriate results
+                cur.execute("SELECT t.TeamName, r.StartTime, r.FinishTime, r.Letters, t.GamePin FROM Teams t, Results r WHERE t.Gamepin=?", (gamePin))
+
+                response = cur.fetchall()
+        except Exception as e:
+            print(e)
+            response = {'status':'0', 'message':'BAD - Unsuccessful'}
+
+        finally:
+
+            # Return the result
+            return response
+
+            con.close()
