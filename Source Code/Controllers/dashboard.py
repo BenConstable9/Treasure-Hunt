aea1be22c2e961f98f1f5b90859ee7e2eac0885a
BenConstable9 <ben-constable@outlook.com>
2020-02-25 14:21:56 +0000

    Revert "Map page has a map with markers"
    This reverts commit 78bb9374fe9e2fcc8cf4ef80831a33d2668df9da.

--- Source Code/Controllers/dashboard.py
+++ Source Code/Controllers/dashboard.py
@@ -3,10 +3,15 @@ from flask import render_template
 from Models.subjectModel import subjectModel
 from Models.questionModel import questionModel
 from Helpers.utility import escapeInput
+import random
 
-# Author - Zach Lavender
-# MVC Controller for the map page
-class MapController():
+# Author - Ben Constable, Zach Lavender
+# MVC Controller for the home page
+class DashboardController():
+
+    def __init__(self):
+        pass
+
     """Handle the empty page.
 
     :return: A redirect or a template. """
@@ -23,16 +28,20 @@ class MapController():
             if response["status"] == "1":
                 data = response["data"]
                 random.shuffle(data)
-                return render_template('map.html',info = data)
+                return render_template('dashboard.html',info = data)
             else:
                 return render_template('home.html')
+    def  building(self):
+        return render_template('building.html')
 
-    def getPins(self):
-        if not session.get('loggedIn'):
-            return redirect("/", code=302)
-        else:
-            subject = session.get('subject')
-            response = questionModel.getQuestions(escapeInput(subject))
-            return response
+    def lecturer(self):
+        return render_template('lecturers.html')
 
-mapController=MapController()
+    def faq(self):
+        return render_template('FAQs.html')
+
+    def openMap(self):
+        return render_template('Map.html')
+
+
+dashboardController=DashboardController()
