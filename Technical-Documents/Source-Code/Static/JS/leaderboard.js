a99f7fdbfc6365ef312c4bd9e56446d1010e504d
adam-bannister <adambannister@ymail.com>
2020-02-26 14:50:12 +0000

    leaderboard

---
+++ Technical-Documents/Source-Code/Static/JS/leaderboard.js
@@ -0,0 +1,13 @@
+/* Author - Adam Bannister
+*/
+
+document.addEventListener('DOMContentLoaded', function(){
+  /* Handle the callback from creating the leaderboard
+
+  :param reponse: the response from the request
+  */
+  function createLeaderboardCallback(response) {
+    if (response.status == "1") {
+      
+  }
+}
